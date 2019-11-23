3bd7ae3ec6d0ea236c2bad271b2f3e2833625f93
jcbanning <32884889+jcbanning@users.noreply.github.com>
2019-11-23 16:36:07 -0500

    added footer and header calls

--- src/components/ChairGeekLoadingPage.js
+++ src/components/ChairGeekLoadingPage.js
@@ -7,6 +7,10 @@ import Logo from '../chair_geek_logo.jpg'
 import LibraryPic from '../petracca_photo_for_news.jpg'
 import {Link} from 'react-router-dom'
 
+//header and footer components
+import TransparentFooter from "/TransparentFooter.js";
+import ExamplesNavBar from "/ExamplesNavbar.js";
+
 const ChairGeekLoadingPage = () =>
     <div>
         <Hero
